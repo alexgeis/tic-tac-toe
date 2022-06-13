0596f8aa5baace023ba5e7a9cd85ba6452ca93bf
Alex Geis <siegxela@gmail.com>
2022-06-13 15:54:28 -0600

    add background color change upon clicking on tic tac toe board

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,20 @@ const renderGameboard = () => {
 	}
 };
 renderGameboard();
+
+const gameSquares = gameboardEl.children;
+const gameSquaresArr = [...gameSquares];
+console.log(gameSquaresArr);
+gameboardEl.addEventListener("click", selectBox);
+
+function selectBox(event) {
+	let element = event.target;
+	let children = element.parentElement.children;
+	let ticDivs = [...children];
+	console.log(element);
+	for (let i = 0; i < ticDivs.length; i++) {
+		const box = ticDivs[i];
+		box.classList.remove(".selected");
+	}
+	element.classList.toggle("selected");
+}
