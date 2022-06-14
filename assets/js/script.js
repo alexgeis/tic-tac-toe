e1af57f9e4b4894dd9dae8444041a555acb868b8
Alex Geis <siegxela@gmail.com>
2022-06-14 12:26:34 -0600

    add check win game logic - WIP to validate computer win

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,7 @@ const renderGameboard = () => {
 };
 
 const gameSquares = gameboardEl.children;
-console.log(gameboardEl.children);
-console.log(gameSquares);
 const gameSquaresArr = [...gameSquares];
-console.log(gameSquaresArr);
 gameboardEl.addEventListener("click", selectBox);
 
 function selectBox(event) {
@@ -54,7 +51,7 @@ function selectBox(event) {
 	element.classList.toggle("selected");
 
 	element.textContent = "X";
-	setCurrentTurn("computer");
+	checkWin();
 }
 
 const clearBtn = document.querySelector("#clear-board");
@@ -63,22 +60,136 @@ clearBtn.addEventListener("click", clearBoard);
 function clearBoard() {
 	for (let i = 0; i < [...gameboardEl.children].length; i++) {
 		const element = [...gameboardEl.children][i];
-		element.classList.remove("selected");
+		element.classList.remove("selected", "comp-selected");
 		element.textContent = "";
 	}
+	clearBtn.textContent = "CLEAR BOARD";
+	gameboardEl.addEventListener("click", selectBox);
 }
 
+const generateNumber0to8 = () => Math.floor(Math.random() * 9);
+let randoNum = generateNumber0to8();
+console.log({ randoNum });
 function computerTurn() {
-	let randoNum = Math.floor(Math.random() * 9);
-	if ([...gameboardEl.children][randoNum].textContent !== "") computerTurn();
+	//validate if gameboard is full
+	//if content exists, push to content array
+	let contentArray = [...gameboardEl.children].filter(
+		(x) => x.textContent === "X" || x.textContent === "O"
+	);
+	if (contentArray.length === 9) return;
+
+	//validate if X or O already exists in targeted square
+	if ([...gameboardEl.children][randoNum].textContent !== "") {
+		randoNum = generateNumber0to8();
+		computerTurn();
+	}
 
 	const compTurnTimer = setTimeout(() => {
+		console.log("comp-selected class added");
 		[...gameboardEl.children][randoNum].classList.add("comp-selected");
 		[...gameboardEl.children][randoNum].textContent = "O";
 		setCurrentTurn("player");
-		clearInterval(compTurnTimer);
-	}, 1000);
+		clearTimeout(compTurnTimer);
+	}, 700);
+	checkWin();
 }
+
+const userScoreDisplay = document.querySelector("#player-score");
+const compScoreDisplay = document.querySelector("#comp-score");
+
+const gameDisplay = document.querySelector("#game-display");
+function checkWin() {
+	if (
+		//PLAYER VALIDATIONS
+		//top row
+		([...gameboardEl.children][0].textContent === "X" &&
+			[...gameboardEl.children][1].textContent === "X" &&
+			[...gameboardEl.children][2].textContent === "X") ||
+		//top left diagonal
+		([...gameboardEl.children][0].textContent === "X" &&
+			[...gameboardEl.children][4].textContent === "X" &&
+			[...gameboardEl.children][8].textContent === "X") ||
+		//left column
+		([...gameboardEl.children][0].textContent === "X" &&
+			[...gameboardEl.children][3].textContent === "X" &&
+			[...gameboardEl.children][6].textContent === "X") ||
+		//middle row
+		([...gameboardEl.children][3].textContent === "X" &&
+			[...gameboardEl.children][4].textContent === "X" &&
+			[...gameboardEl.children][5].textContent === "X") ||
+		//middle column
+		([...gameboardEl.children][1].textContent === "X" &&
+			[...gameboardEl.children][4].textContent === "X" &&
+			[...gameboardEl.children][7].textContent === "X") ||
+		//top right diagonal
+		([...gameboardEl.children][2].textContent === "X" &&
+			[...gameboardEl.children][4].textContent === "X" &&
+			[...gameboardEl.children][6].textContent === "X") ||
+		//right column
+		([...gameboardEl.children][2].textContent === "X" &&
+			[...gameboardEl.children][5].textContent === "X" &&
+			[...gameboardEl.children][6].textContent === "X") ||
+		//bottom row
+		([...gameboardEl.children][6].textContent === "X" &&
+			[...gameboardEl.children][7].textContent === "X" &&
+			[...gameboardEl.children][8].textContent === "X")
+	) {
+		gameDisplay.textContent = `You win hot stuff!`;
+		const newUserScore = currentUserScore++;
+		setCurrentUserScore(newUserScore);
+		userScoreDisplay.textContent = `Score: ${currentUserScore}`;
+		gameboardEl.removeEventListener("click", selectBox);
+		clearBtn.textContent = "RESET BOARD";
+		return true;
+	} else if (
+		//COMPUTER VALIDATIONS
+		//top row
+		([...gameboardEl.children][0].textContent === "O" &&
+			[...gameboardEl.children][1].textContent === "O" &&
+			[...gameboardEl.children][2].textContent === "O") ||
+		//top left diagonal
+		([...gameboardEl.children][0].textContent === "O" &&
+			[...gameboardEl.children][4].textContent === "O" &&
+			[...gameboardEl.children][8].textContent === "O") ||
+		//left column
+		([...gameboardEl.children][0].textContent === "O" &&
+			[...gameboardEl.children][3].textContent === "O" &&
+			[...gameboardEl.children][6].textContent === "O") ||
+		//middle row
+		([...gameboardEl.children][3].textContent === "O" &&
+			[...gameboardEl.children][4].textContent === "O" &&
+			[...gameboardEl.children][5].textContent === "O") ||
+		//middle column
+		([...gameboardEl.children][1].textContent === "O" &&
+			[...gameboardEl.children][4].textContent === "O" &&
+			[...gameboardEl.children][7].textContent === "O") ||
+		//top right diagonal
+		([...gameboardEl.children][2].textContent === "O" &&
+			[...gameboardEl.children][4].textContent === "O" &&
+			[...gameboardEl.children][6].textContent === "O") ||
+		//right column
+		([...gameboardEl.children][2].textContent === "O" &&
+			[...gameboardEl.children][5].textContent === "O" &&
+			[...gameboardEl.children][6].textContent === "O") ||
+		//bottom row
+		([...gameboardEl.children][6].textContent === "O" &&
+			[...gameboardEl.children][7].textContent === "O" &&
+			[...gameboardEl.children][8].textContent === "O")
+	) {
+		gameDisplay.textContent = `Computer wins ya dingus!`;
+		const newCompScore = currentCompScore++;
+		setCurrentCompScore(newCompScore);
+		compScoreDisplay.textContent = `Score: ${currentCompScore}`;
+		gameboardEl.removeEventListener("click", selectBox);
+		clearBtn.textContent = "RESET BOARD";
+		return false;
+	} else {
+		if (currentTurn === "player") setCurrentTurn("computer");
+		console.log("enter check win else");
+		// else
+	}
+}
+
 window.onload = () => {
 	renderGameboard();
 	// setupGrid(DEFAULT_SIZE);
